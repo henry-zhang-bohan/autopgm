0f9c406ebaecf22e153a16822eb89296e13e7000
Bohan Zhang <henryzhangbh@outlook.com>
2018-02-03 14:06:14 -0500

    minimal setup structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='autopgm',
+      version='0.1',
+      description='Automatically learn Bayesian Networks from multiple data sources',
+      url='https://github.com/ideo-henry/autopgm',
+      author='Bohan Zhang',
+      author_email='henryhenry.zhang@mail.utoronto.ca',
+      license='',
+      packages=['autopgm'],
+      zip_safe=False)
