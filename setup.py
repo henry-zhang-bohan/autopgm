264546d6694e2b1bc0e0ef11ce58d2bc44edca79
Bohan Zhang <henryzhangbh@outlook.com>
2018-02-03 14:17:44 -0500

    updated dependencies

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ setup(name='autopgm',
       url='https://github.com/ideo-henry/autopgm',
       author='Bohan Zhang',
       author_email='henryhenry.zhang@mail.utoronto.ca',
-      license='',
+      license='MIT',
       packages=['autopgm'],
+      install_requires=[
+          'scipy >= 0.19.1',
+          'numpy >= 1.14.0',
+          'pandas >= 0.20.3',
+          'pgmpy'
+      ],
       zip_safe=False)
